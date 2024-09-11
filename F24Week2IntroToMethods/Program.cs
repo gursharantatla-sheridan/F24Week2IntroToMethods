1b81e32f6b70db9a00dbc6d54ebc5eb2e721d2be
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-11 16:47:50 -0400

    output parameters

--- F24Week2IntroToMethods/Program.cs
+++ F24Week2IntroToMethods/Program.cs
@@ -13,15 +13,35 @@
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
 
-            ChangeValue(n, s, a);
+            ChangeValue(ref n, s, a);
 
             Console.WriteLine("\nAfter changes:");
             Console.WriteLine("n = " + n);
             Console.WriteLine("s = " + s);
             Console.WriteLine("a[0] = " + a[0]);
+
+            Console.WriteLine("\n\n");
+
+
+
+            int p = 4, q = 6;
+            int sum, mul;
+
+            Calculate(p, q, out sum, out mul);
+
+            Console.WriteLine("Sum = " + sum);
+            Console.WriteLine("Mul = " + mul);
         }
 
-        static void ChangeValue(int num, string str, int[] arr)
+
+        static void Calculate(int x, int y, out int sum, out int mul)
+        {
+            sum = x + y;
+            mul = x * y;
+        }
+
+
+        static void ChangeValue(ref int num, string str, int[] arr)
         {
             num = 100;
             str = "bye";
