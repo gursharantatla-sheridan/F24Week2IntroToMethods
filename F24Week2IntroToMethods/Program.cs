1234bfeaf3faa021e5f98cb525e293b08ccc0095
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-11 16:32:00 -0400

    value and ref types

--- F24Week2IntroToMethods/Program.cs
+++ F24Week2IntroToMethods/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 5;
+            string s = "hello";
+            int[] a = { 4, 3, 6, 5, 2, 7, 8 };
+
+            Console.WriteLine("Before changes:");
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("a[0] = " + a[0]);
+
+            ChangeValue(n, s, a);
+
+            Console.WriteLine("\nAfter changes:");
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("a[0] = " + a[0]);
+        }
+
+        static void ChangeValue(int num, string str, int[] arr)
+        {
+            num = 100;
+            str = "bye";
+            arr[0] = 100;
         }
     }
 }
